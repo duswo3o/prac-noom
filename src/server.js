d8e9837f687d37e3c3a717941163a974b18ea4d9
yeonjae <pyj3740@gmail.com>
2024-12-14 18:40:06 +0900

    websocket in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -12,6 +14,13 @@ app.use("/public", express.static(__dirname + "/public"));
 
 // home으로 가면 request, response를 받고 res.render를 함(home을 렌더)
 app.get("/", (req, res) => res.render("home"));
+// home 이외의 url로 이동하면 home으로 redirect
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app); // http 서버
+const wss = new WebSocket.Server({server}) // ws 서버
+
+server.listen(3000, handleListen)
