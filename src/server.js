46bafab61f3ca636a4eb2125828ef2372eca6771
yeonjae <pyj3740@gmail.com>
2024-12-15 01:24:40 +0900

    Nickname part Two

--- src/server.js
+++ src/server.js
@@ -28,10 +28,20 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anonymous"; // 닉네임을 설정하지 않은 유저들에 대해 기본값 설정
   console.log("Connected to Browser ✅");
   socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-  socket.on("message", (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message.toString("utf-8")));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg.toString("utf-8"));
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname} : ${message.payload}`)
+        );
+      case "nickname":
+        // console.log(message.payload);
+        socket["nickname"] = message.payload;
+    }
   });
 });
 
