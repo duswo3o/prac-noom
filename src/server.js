8101a36620324163fa071a8a76439878ba2a5bff
yeonjae <pyj3740@gmail.com>
2024-12-15 21:08:37 +0900

    Nicknames

--- src/server.js
+++ src/server.js
@@ -26,23 +26,25 @@ const httpServer = http.createServer(app); // http 서버
 const wsServer = SocketIO(httpServer); // socket IO 서버
 
 wsServer.on("connection", (socket) => {
+  socket["nickname"] = "Anonymous";
   socket.onAny((event) => {
     console.log(`Socket Event:${event}`);
   });
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome");
+    socket.to(roomName).emit("welcome", socket.nickname);
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) => {
-      socket.to(room).emit("bye");
+      socket.to(room).emit("bye", socket.nickname);
     });
   });
   socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", msg);
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
     done();
   });
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
 // // fake Database
