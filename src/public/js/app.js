6fc9f1704909d79a1fc0ba72d773d590e0cfea87
yeonjae <pyj3740@gmail.com>
2024-12-16 13:54:52 +0900

    Call controls

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,18 +3,43 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
 
 let myStream;
-let muted = true;
+let muted = false;
 let cameraOff = false;
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    // console.log(cameras);
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      cameraSelect.appendChild(option);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 async function getMedia() {
   try {
     myStream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
+      audio: true,
       video: true,
     });
+    // off 상태로 시작하기
+    myStream.getAudioTracks().forEach((track) => {
+      track.enabled = false;
+    });
+    myStream.getVideoTracks().forEach((track) => {
+      track.enabled = false;
+    });
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (error) {
     console.log(error);
   }
@@ -23,6 +48,10 @@ async function getMedia() {
 getMedia();
 
 function handleMuteClick() {
+  // console.log(myStream.getAudioTracks());
+  myStream.getAudioTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -33,6 +62,10 @@ function handleMuteClick() {
 }
 
 function handleCameraClick() {
+  // console.log(myStream.getVideoTracks());
+  myStream.getVideoTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
   if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
